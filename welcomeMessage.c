f78035b38a85ee961ec8910d813d2906921abd73
InemGIBSON <inemgibs@gmail.com>
2024-10-29 07:43:40 +0000

    Using fgets to collect input

--- welcomeMessage.c
+++ welcomeMessage.c
@@ -4,8 +4,10 @@ int main() {
 char name[50]; //This will store username
 
 printf("Enter your name: ");
-scanf("%49s", name); // This will limit the user input to ensure that there is not overflow.
 
+// scanf("%49s", name); This will limit the user input to ensure that there is not overflow.
+
+fgets(name, sizeof(name), stdin); // this takes the username in full
 printf("Welcome , %s!\n", name);
 
 return 0;
